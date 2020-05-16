d3d8ab3041b49139d50bd44b5c3fc6b9e85e871a
Cliff Harvey <cs.hbar@gmail.com>
2020-05-16 18:22:34 +0200

    Provide export conveniences

--- openapi/src/OpenAPI.hs
+++ openapi/src/OpenAPI.hs
@@ -16,9 +16,32 @@ module OpenAPI
   , SchemaEnv
 
   -- * Types
-  , module OpenAPI.Internal.Types 
+  , module OpenAPI.Internal.Types
+
+  -- * Conveniences
+  , renderSchemaYaml
+  , renderSchemaJson
+  , writeSchemaYaml
+  , writeSchemaJson
   ) where
 
 import OpenAPI.Internal.Class
 import OpenAPI.Internal.Types
 import OpenAPI.Internal.References
+
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Lazy.Char8 as BSL
+import qualified Data.Yaml as Yaml
+import qualified Data.Aeson as Aeson
+
+renderSchemaYaml :: OpenAPI -> BS.ByteString
+renderSchemaYaml = Yaml.encode
+
+renderSchemaJson :: OpenAPI -> BSL.ByteString
+renderSchemaJson = Aeson.encode
+
+writeSchemaYaml :: FilePath -> OpenAPI -> IO ()
+writeSchemaYaml path = BS.writeFile path . renderSchemaYaml
+
+writeSchemaJson :: FilePath -> OpenAPI -> IO ()
+writeSchemaJson path = BSL.writeFile path . renderSchemaJson
